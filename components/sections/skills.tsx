af22bc0266859a8828c1cf0853cf6adedb5c30e0
Carson Davis <cmd52465@gmail.com>
2026-06-04 18:29:49 -0700

    fix: resolve image dimensions warnings

--- components/sections/skills.tsx
+++ components/sections/skills.tsx
@@ -6,14 +6,15 @@ export function Skills() {
 	return (
 		<div className="px-6 md:pl-56 md:pr-12 lg:pl-64 lg:pr-20">
 			<Marquee duration={30} direction="right">
-				{skills.map((skill) => (
+				{skills.map((skill, i) => (
 					<Image
 						key={skill.name}
 						src={skill.image}
 						alt={skill.name}
 						width={60}
 						height={60}
-						className="mx-6 object-contain"
+						priority={i === 0}
+						className="mx-6 object-contain w-auto h-auto"
 					/>
 				))}
 			</Marquee>
