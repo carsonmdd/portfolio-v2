df2df9dd200f03e6b6bebc6529ad09c004d3e954
Carson Davis <cmd52465@gmail.com>
2026-06-07 19:55:54 -0700

    feat: enable analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Sidebar } from '@/components/sidebar';
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -32,6 +33,7 @@ export default function RootLayout({
 			<body className="min-h-full flex flex-col bg-black text-white">
 				<Sidebar />
 				<main>{children}</main>
+				<Analytics />
 			</body>
 		</html>
 	);
